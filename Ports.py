2a48165caaab28f8b931cc4337e500b8ba28f1bb
dromazan <dromazan@gmail.com>
2017-03-02 18:34:33 +0200

    upd

--- Ports.py
+++ Ports.py
@@ -6,6 +6,8 @@ from dask.compatibility import apply
 
 root = Tk()
 
+portstate = "Disconnected"
+
 
 def get_serial_ports_list():
     if sys.platform.startswith('win'):
@@ -33,8 +35,8 @@ ports = get_serial_ports_list()
 print(ports)
 
 root.geometry("%dx%d+%d+%d" % (330, 200, 200, 150))
-label_serial = Label(root, text='Select Serial Port:')
-label_serial.pack()
+label_serial = Label(root, text='Select Serial Port:').grid(row=0, column=0)
+#label_serial.pack()
 var = StringVar(root)
 if ports:
     var.set(ports[0])
@@ -42,9 +44,16 @@ else:
     var.set('null')
     ports=['null']
 ports_list = apply(OptionMenu, (root, var) + tuple(ports))
-ports_list.pack()
+ports_list.grid(row=0, column=1)
 
 ser = serial.Serial()
+
+
+def set_port_state_label(state):
+    if state == "Connected":
+        Label(root, text=state, fg='green').grid(row=1, column=1)
+    else:
+        Label(root, text=state, fg='red').grid(row=1, column=1)
 
 
 def open_port():
@@ -53,24 +62,28 @@ def open_port():
     ser.open()
     if ser.is_open:
         print(var.get()+" is opened")
+        set_port_state_label("Connected")
     pass
 
 
-open_btn = Button(root, text="Open port", command=open_port)
-open_btn.pack()
+open_btn = Button(root, text="Open port", command=open_port).grid(row=0,column=2)
+#open_btn.pack()
 
 
 def close_port():
     if ser.is_open:
         ser.close()
         print(var.get()+" closed")
+        set_port_state_label("Disconnected")
     else:
         print("port is not opened")
     pass
 
 
-close_btn = Button(root, text="Close port", command=close_port)
-close_btn.pack()
+close_btn = Button(root, text="Close port", command=close_port).grid(row=0, column=3)
+#close_btn.pack()
+
+state = Label(root, text="Port state: ").grid(row=1)
 
 
 def read_port_value():
@@ -79,15 +92,11 @@ def read_port_value():
         print(portval)
 
     else:
-        portval = "null1"
+        portval = "null"
         print(portval)
     return portval
 
-port_val = Button(root, text="Read port", command=read_port_value)
-port_val.pack()
-
-port_val_label = Label(root, textvariable=read_port_value)
-port_val_label.pack()
-
+port_val = Button(root, text="Read port", command=read_port_value).grid(row=2, column=0)
+#port_val.pack()
 
 root.mainloop()
