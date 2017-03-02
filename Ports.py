b693d797dc68ab3fe30f0780dbcc6272e2be8279
dromazan <dromazan@gmail.com>
2017-03-02 13:29:39 +0200

    upd

--- Ports.py
+++ Ports.py
@@ -4,10 +4,8 @@ import glob
 
 root = Tk()
 
-
 def get_serial_ports_list():
     if sys.platform.startswith('win'):
-        print(sys.platform.title())
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
         # this excludes your current terminal "/dev/tty"
@@ -31,18 +29,17 @@ def get_serial_ports_list():
 ports = get_serial_ports_list()
 print(ports)
 
-root.geometry("%dx%d+%d+%d" % (300, 200, 30, 30))
-label_serial = Label(root, text='Select Serial Port:').grid(row=0, sticky=W)
-# label_serial.pack()
+root.geometry("%dx%d+%d+%d" % (330, 200, 200, 150))
+label_serial = Label(root, text='Select Serial Port:')
+label_serial.pack()
 var = StringVar(root)
 if ports:
     var.set(ports[0])
 else:
     var.set('null')
-    ports = ['null']
-
-ports_list = OptionMenu(root, var, tuple(ports)).grid(row=0, column=1, sticky=W)
-# ports_list.pack(side=LEFT)
+    ports=['null']
+ports_list = OptionMenu(root, var, ports)
+ports_list.pack()
 
 ser = serial.Serial()
 
@@ -51,33 +48,43 @@ def open_port():
     ser.baudrate = 19200
     ser.port = var.get()
     ser.open()
+    if ser.is_open:
+        print(var.get()+" is opened")
     pass
 
-if ser.is_open:
-    connected = Label(root, text="Connected", fg='green')
 
-
-    def request_temp():
-        temp = ser.readline()
-        temp_str = str(temp)
-        return temp_str
-
-
-    temp_label = Label(root, text=request_temp(), font="arial 14").grid(row=4)
-
-else:
-    connected = Label(root, text="Disconnected", fg='red')
-    temp_label = Label(root, text="NULL", font="arial 14").grid(row=4)
-
-connected.grid(row=1)
-open_port_btn = Button(root, text="Open Port", command=open_port).grid(row=0, column=2)
+open_btn = Button(root, text="Open port", command=open_port)
+open_btn.pack()
 
 
 def close_port():
-    ser.close()
+    if ser.is_open:
+        ser.close()
+        print(var.get()+" closed")
+    else:
+        print("port is not opened")
     pass
 
 
-close_port_btn = Button(root, text="Close Port", command=close_port).grid(row=0, column=3)
+close_btn = Button(root, text="Close port", command=close_port)
+close_btn.pack()
+
+
+def read_port_value():
+    if ser.is_open:
+        portval = ser.readline()
+        print(portval)
+
+    else:
+        portval = "null1"
+        print(portval)
+    return portval
+
+port_val = Button(root, text="Read port", command=read_port_value)
+port_val.pack()
+
+port_val_label = Label(root, textvariable=read_port_value)
+port_val_label.pack()
+
 
 root.mainloop()
